9faec630b5bdec82dfbe62248aeed764367bccb8
Iago González <iago.gc88@gmail.com>
2020-12-22 22:49:45 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleAppT02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppT02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); // Para la ejecución hasta presionar tecla
+        }
+    }
+}
